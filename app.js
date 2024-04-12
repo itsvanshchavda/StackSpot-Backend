d77a3400c9f95e2380094a2ba0b92c4f3b7cefa5
Vansh Chavda <vanshchavda111@gmail.com>
2024-04-12 16:37:36 +0530

    CORS Fixed

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 // CORS
 const corsOptions = {
-  origin: ["http://localhost:5173" , "https://stack-spot.vercel.app/"],
+  origin: ["http://localhost:5173" , "https://stack-spot.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
